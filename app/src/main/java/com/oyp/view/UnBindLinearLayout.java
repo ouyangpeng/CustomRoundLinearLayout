05892862de866b843b43f04c9abf7161585f2f3c
ouyangpeng <ouyangpeng@oaserver.dw.gdbbk.com>
2018-12-26 21:04:12 +0800

    1、实现 ui的要求  自定义LinearLayout，绘制好背景

--- app/src/main/java/com/oyp/view/UnBindLinearLayout.java
+++ app/src/main/java/com/oyp/view/UnBindLinearLayout.java
@@ -11,15 +11,30 @@ import android.util.AttributeSet;
 import android.widget.LinearLayout;
 
 public class UnBindLinearLayout extends LinearLayout {
+    /**
+     * 大圆、小圆的圆心的Y坐标
+     */
     private int circleStartY;
+    /**
+     * 第一个小圆的圆心的X坐标
+     */
     private int smallCircleStartX;
+    /**
+     * 小圆的个数
+     */
+    private int smallCircleCount;
+    /**
+     * 小圆的半径
+     */
     private int smallCircleRadius;
+    /**
+     * 小圆之间的间距
+     */
     private int smallCircleMargin;
+    /**
+     * 大圆的半径
+     */
     private int bigCircleRadius;
-    private int leftBigCircleStartX;
-    private int leftBigCircleStartY;
-    private int rightBigCircleStartX;
-    private int rightBigCircleStartY;
 
     public UnBindLinearLayout(Context context) {
         this(context, null);
@@ -36,34 +51,31 @@ public class UnBindLinearLayout extends LinearLayout {
     @TargetApi(21)
     public UnBindLinearLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-
+        smallCircleCount = 19;
         circleStartY = SizeConvertUtil.dpTopx(context, 80);
 
-        smallCircleStartX = SizeConvertUtil.dpTopx(context, 20);
+        smallCircleStartX = SizeConvertUtil.dpTopx(context, 18);
         smallCircleRadius = SizeConvertUtil.dpTopx(context, 2);
-        smallCircleMargin = SizeConvertUtil.dpTopx(context, 16);
 
-        leftBigCircleStartX = SizeConvertUtil.dpTopx(context, 8);
-        rightBigCircleStartX = SizeConvertUtil.dpTopx(context, 318);
         bigCircleRadius = SizeConvertUtil.dpTopx(context, 8);
 
         setWillNotDraw(false);//设置调用onDraw方法
-
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        //制成一个白色的圆角矩形  作为背景
+
+        //制成一个白色的圆角矩形  作为背景  画2个半圆 和 18个小圆  作为分隔线
+        // ==========================    第一步、绘制白色矩形
         RectF backGroundRectF = new RectF(0, 0, canvas.getWidth(), canvas.getHeight());
         Paint backGroundPaint = new Paint();
         backGroundPaint.setColor(Color.WHITE);
         canvas.drawRoundRect(backGroundRectF, 30, 30, backGroundPaint);
 
-        // 画2个半圆 和 18个小圆  作为分隔线
+        // ==========================    第二步、绘制2个半圆
         Paint circlePaint = new Paint();
         circlePaint.setStyle(Paint.Style.FILL);
-        // 1.画2个半圆
         circlePaint.setColor(Color.parseColor("#f1f1f1"));
         // 圆弧的外轮廓矩形区域
         RectF leftOval = new RectF(-bigCircleRadius, circleStartY - bigCircleRadius,
@@ -80,20 +92,18 @@ public class UnBindLinearLayout extends LinearLayout {
         canvas.drawArc(leftOval, -90, 180, true, circlePaint);
 
         //右边的半圆
-        RectF rightOval = new RectF(getWidth() - bigCircleRadius, circleStartY - bigCircleRadius,
+        RectF rightOval = new RectF(canvas.getWidth() - bigCircleRadius, circleStartY - bigCircleRadius,
                 getWidth() + bigCircleRadius, circleStartY + bigCircleRadius);
         canvas.drawArc(rightOval, 90, 180, true, circlePaint);
 
-
-        // 2.画18个小圆  作为分隔线
-
+        // ==========================    第三步、绘制18个小圆  作为分隔线
+        // 最后一个小圆和第一个小圆之间 有17段分隔空白
+        smallCircleMargin = ((canvas.getWidth() - smallCircleStartX) - (smallCircleStartX)) / (smallCircleCount - 1);
         circlePaint.setColor(Color.parseColor("#dddddd"));
-        for (int i = 0; i < 18; i++) {
+        for (int i = 0; i < smallCircleCount; i++) {
             canvas.drawCircle(smallCircleStartX + i * smallCircleMargin, circleStartY, smallCircleRadius, circlePaint);
         }
-
-
-        //让RelativeLayout绘制自己
+        // ==========================    第四步、 让RelativeLayout绘制自己
         super.onDraw(canvas);
     }
 
